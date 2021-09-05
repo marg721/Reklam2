31e462aa65406704816cd5b81d07d272dc6d0900
marg721 <87083389+marg721@users.noreply.github.com>
2021-09-05 21:37:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`marg🖤`) 
+client.user.setGame(``) 
 });
 
 
